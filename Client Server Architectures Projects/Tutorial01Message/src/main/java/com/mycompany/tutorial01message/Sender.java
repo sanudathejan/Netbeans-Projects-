2622837d7b0e9ffb3c8a1d450669f8d524c20827
sanudathejan <thejansanuda2006@gmail.com>
2026-01-27 11:56:58 +0530

    Replace System.out with Logger in Sender class
    Updated the Sender class to use java.util.logging.Logger instead of System.out.println for logging message creation and sending events. This improves logging consistency and allows for better log management.

--- Client Server Architectures Projects/Tutorial01Message/src/main/java/com/mycompany/tutorial01message/Sender.java
+++ Client Server Architectures Projects/Tutorial01Message/src/main/java/com/mycompany/tutorial01message/Sender.java
@@ -8,19 +8,21 @@ package com.mycompany.tutorial01message;
  *
  * @author Sanuda
  */
+import java.util.logging.Logger;
+
 public class Sender {
 
-    // Create a message
+    private static final Logger logger = Logger.getLogger(Sender.class.getName());
+
     public Message createMessage(String content) {
+        logger.info("Sender: Creating message...");
         Message message = new Message(content);
-        System.out.println("Sender: Message created.");
+        logger.info("Sender: Message created successfully.");
         return message;
     }
 
-    // Send a message to Receiver
     public void sendMessage(Message message, Receiver receiver) {
-        System.out.println("Sender: Sending message to receiver...");
+        logger.info("Sender: Sending message to Receiver...");
         receiver.receiveMessage(message);
     }
 }
-
