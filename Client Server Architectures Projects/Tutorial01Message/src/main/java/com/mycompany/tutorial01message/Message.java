3ddca74204289910ebaca6e2f30aab3b536ebabd
sanudathejan <thejansanuda2006@gmail.com>
2026-01-27 11:19:58 +0530

    Add Message class with content field
    Introduces the Message class containing a content field, constructor, and getter method. This class will be used to encapsulate message data in the application.

---
+++ Client Server Architectures Projects/Tutorial01Message/src/main/java/com/mycompany/tutorial01message/Message.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tutorial01message;
+
+/**
+ *
+ * @author Sanuda
+ */
+public class Message {
+    private String content;
+
+    // Constructor
+    public Message(String content) {
+        this.content = content;
+    }
+
+    // Getter method
+    public String getContent() {
+        return content;
+    }
+}
+
+  
