3c185f7246df4a07079cb588813abdfe3f0d30cf
sanudathejan <thejansanuda2006@gmail.com>
2026-02-03 11:32:15 +0530

    Add User class with name and age fields
    Introduces a User class containing name and age properties, along with corresponding getters and a toString method for representation.

---
+++ Client Server Architectures Projects/Tutorial 02/Week02_ClientServer/src/User.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sanuda
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', age=" + age + "}";
+    }
+}
