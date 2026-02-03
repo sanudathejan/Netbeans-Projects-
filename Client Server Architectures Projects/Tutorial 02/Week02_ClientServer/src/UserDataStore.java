2a23b7f7368f31ea787381dada13ed3f0aa4aa31
sanudathejan <thejansanuda2006@gmail.com>
2026-02-03 11:32:10 +0530

    Add UserDataStore class for managing users
    Introduces the UserDataStore class to manage a list of User objects, providing methods to add users and retrieve all users. This utility will help centralize user data management in the application.

---
+++ Client Server Architectures Projects/Tutorial 02/Week02_ClientServer/src/UserDataStore.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sanuda
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDataStore {
+    private List<User> users;
+
+    public UserDataStore() {
+        users = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users);   // return copy for safety
+    }
+}
+
