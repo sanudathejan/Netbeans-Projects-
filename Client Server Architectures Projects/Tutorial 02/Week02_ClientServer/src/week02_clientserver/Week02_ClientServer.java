66389a415301d0cde2fc5a11dba6fc2214969324
sanudathejan <thejansanuda2006@gmail.com>
2026-02-03 11:31:51 +0530

    Add initial Week02_ClientServer main class
    Created the Week02_ClientServer.java file with a basic main class structure and placeholder for application logic.

---
+++ Client Server Architectures Projects/Tutorial 02/Week02_ClientServer/src/week02_clientserver/Week02_ClientServer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week02_clientserver;
+
+/**
+ *
+ * @author Sanuda
+ */
+public class Week02_ClientServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
