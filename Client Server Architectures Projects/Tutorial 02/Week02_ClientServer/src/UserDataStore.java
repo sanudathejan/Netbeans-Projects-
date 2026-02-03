3609f6245ddce12a7eab01a2a4f288c352503458
sanudathejan <thejansanuda2006@gmail.com>
2026-02-03 11:55:24 +0530

    Fix formatting in UserDataStore.java
    Adjusted whitespace and formatting in the UserDataStore class for improved code readability. No functional changes were made.

--- Client Server Architectures Projects/Tutorial 02/Week02_ClientServer/src/UserDataStore.java
+++ Client Server Architectures Projects/Tutorial 02/Week02_ClientServer/src/UserDataStore.java
@@ -7,16 +7,16 @@
  *
  * @author Sanuda
  */
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; 
+import java.util.List; 
 
 public class UserDataStore {
     private List<User> users;
-
+ 
     public UserDataStore() {
-        users = new ArrayList<>();
+        users = new ArrayList<>(); 
     }
-
+    
     public void addUser(User user) {
         users.add(user);
     }
